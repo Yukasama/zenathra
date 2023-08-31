1955b6277e0dc8b1ab9700bbd6b75348f9b46ab3
Yukasama <yukasamaa@gmail.com>
2023-08-31 20:16:21 +0200

    Updating Screener...

--- src/app/screener/page.tsx
+++ src/app/screener/page.tsx
@@ -16,13 +16,26 @@ import {
 import { Screener } from "@/types/stock";
 import { Prisma, Stock } from "@prisma/client";
 import { Button } from "@/components/ui/button";
-import SelectInput from "@/components/ui/select-input";
 import { StructureProps } from "@/types/layout";
 import { BarChart2, FileText, Layers, RotateCcw } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import debounce from "lodash.debounce";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { SelectContent } from "@radix-ui/react-select";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Page() {
   const [active, setActive] = useState<string>("Descriptive");
@@ -196,101 +209,101 @@ export default function Page() {
     return <ScreenerItemStructure isLoading />;
   }
 
-  const ratings: any = {
-    Descriptive: <FileText />,
-    Fundamental: <Layers />,
-    Technical: <BarChart2 />,
-  };
-
   return (
-    <div className="grid h-full grid-cols-8">
-      <Tabs defaultValue="descriptive" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="descriptive">Descriptive</TabsTrigger>
-        <TabsTrigger value="fundamental">Fundamental</TabsTrigger>
-        <TabsTrigger value="technical">Technical</TabsTrigger>
-      </TabsList>
-      <TabsContent value="descriptive">
-        {descriptive.map((filter) => (
-          <div key={filter.id} className="mb-4">
-            <SelectInput
-              label={filter.label}
-              options={filter.options}
-              setOption={filter.setOption}
-              resetCounter={resetCounter}
-            />
-          </div>
-        ))}
-      </TabsContent>
+    <div className="flex">
+      <Tabs defaultValue="descriptive">
+        <TabsList>
+          <TabsTrigger value="descriptive">
+            <FileText />
+            Descriptive
+          </TabsTrigger>
+          <TabsTrigger value="fundamental">
+            <Layers />
+            Fundamental
+          </TabsTrigger>
+          <TabsTrigger value="technical">
+            <BarChart2 />
+            Technical
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="descriptive">
+          <Card>
+            <CardHeader>
+              <CardTitle>Descriptive</CardTitle>
+              <CardDescription>Filters that describe the stock</CardDescription>
+            </CardHeader>
+            <CardContent className="f-col gap-4">
+              {descriptive.map((filter) => (
+                <Select key={filter.id}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Any" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {filter.options.map((option) => (
+                      <SelectItem
+                        key={option}
+                        value={option}
+                        onChange={() => filter.setOption(option)}>
+                        {option}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              ))}
+            </CardContent>
+          </Card>
+        </TabsContent>
       </Tabs>
-      <div className="col-span-4 overflow-auto lg:col-span-5 xl:col-span-6">
-        <div>
-          <div className="sticky top-0 mb-0.5 grid grid-cols-10 gap-4 bg-slate-100/80 p-3 px-8 dark:bg-zinc-300/80">
-            <p className="col-span-3 rounded-md bg-blue-500 p-1.5 px-3 font-medium text-white">
-              Company
-            </p>
-            <p className="col-span-1 rounded-md bg-blue-500 p-1.5 px-3 font-medium text-white">
-              Change
-            </p>
-            <p className="col-span-1 rounded-md bg-blue-500 p-1.5 px-3 font-medium text-white">
-              P/E Ratio
-            </p>
-            <p className="col-span-2 rounded-md bg-blue-500 p-1.5 px-3 font-medium text-white">
-              Sector
-            </p>
+      <div className="f-col mb-5 mt-1 gap-2 px-1 lg:px-8">
+        {isFetching ? (
+          <>
+            {[...Array(15)].map((_, i) => (
+              <ScreenerItemLoading key={i} />
+            ))}
+          </>
+        ) : isFetched && !results?.length ? (
+          <p>No Stocks matching the query</p>
+        ) : (
+          <div className="f-col hidden-scrollbar h-[800px] gap-2 overflow-scroll">
+            {results?.map((stock: Stock) => (
+              <Link key={stock.symbol} href={`/stocks/${stock.symbol}`}>
+                <ScreenerItemStructure>
+                  <div className="col-span-3 flex items-center gap-4">
+                    <div className="f-box h-10 w-10 rounded-sm dark:bg-white">
+                      <Image
+                        src={stock.image || "/images/stock.jpg"}
+                        className="max-h-10 w-10 rounded-md p-1 dark:bg-white"
+                        height={30}
+                        width={30}
+                        alt="Logo"
+                        loading="lazy"
+                      />
+                    </div>
+                    <div className="f-col">
+                      <p className="font-medium">{stock.symbol}</p>
+                      <p className="text-sm text-slate-700">
+                        {stock.companyName}
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    className={`p-1.5 ${
+                      Number(stock.changes) > 0
+                        ? "text-green-400"
+                        : "text-red-400"
+                    }  col-span-1 font-medium`}>
+                    {Number(stock.changes) > 0 && "+"}
+                    {stock.changes}
+                  </div>
+                  <div className="col-span-1 p-1.5">
+                    {stock.peRatioTTM ? stock.peRatioTTM.toFixed(2) : 0}
+                  </div>
+                  <div className="col-span-2 p-1.5">{stock.sector}</div>
+                </ScreenerItemStructure>
+              </Link>
+            ))}
           </div>
-          <div className="f-col mb-5 mt-1 gap-2 px-1 lg:px-8">
-            {isFetching ? (
-              <>
-                {[...Array(15)].map((_, i) => (
-                  <ScreenerItemLoading key={i} />
-                ))}
-              </>
-            ) : isFetched && !results?.length ? (
-              <p>No Stocks matching the query</p>
-            ) : (
-              <div className="f-col hidden-scrollbar h-[800px] gap-2 overflow-scroll">
-                {results?.map((stock: Stock) => (
-                  <Link key={stock.symbol} href={`/stocks/${stock.symbol}`}>
-                    <ScreenerItemStructure>
-                      <div className="col-span-3 flex items-center gap-4">
-                        <div className="f-box h-10 w-10 rounded-sm dark:bg-white">
-                          <Image
-                            src={stock.image || "/images/stock.jpg"}
-                            className="max-h-10 w-10 rounded-md p-1 dark:bg-white"
-                            height={30}
-                            width={30}
-                            alt="Logo"
-                            loading="lazy"
-                          />
-                        </div>
-                        <div className="f-col">
-                          <p className="font-medium">{stock.symbol}</p>
-                          <p className="text-sm text-slate-700">
-                            {stock.companyName}
-                          </p>
-                        </div>
-                      </div>
-                      <div
-                        className={`p-1.5 ${
-                          Number(stock.changes) > 0
-                            ? "text-green-400"
-                            : "text-red-400"
-                        }  col-span-1 font-medium`}>
-                        {Number(stock.changes) > 0 && "+"}
-                        {stock.changes}
-                      </div>
-                      <div className="col-span-1 p-1.5">
-                        {stock.peRatioTTM ? stock.peRatioTTM.toFixed(2) : 0}
-                      </div>
-                      <div className="col-span-2 p-1.5">{stock.sector}</div>
-                    </ScreenerItemStructure>
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
